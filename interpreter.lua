827b6785dce542dac1d3322d6890a19beb9aad01
Matthew Robbetts <mrobbetts@gmail.com>
2022-09-23 11:43:12 +0100

    bapl1_l1_ex_matching-the-whole-subject
    Modify the previous exercise so that it only succeeds if it matches the whole subject.

--- interpreter.lua
+++ interpreter.lua
@@ -1,1 +1,1 @@
-p = lpeg.P(" ")^0 * lpeg.C(lpeg.R("09")^1) * lpeg.P(" ")^0 * (lpeg.Cp() * lpeg.P("+") * lpeg.P(" ")^0 * lpeg.C(lpeg.R("09")^1) * lpeg.P(" ")^0)^0
+p = lpeg.P(" ")^0 * lpeg.C(lpeg.R("09")^1) * lpeg.P(" ")^0 * (lpeg.Cp() * lpeg.P("+") * lpeg.P(" ")^0 * lpeg.C(lpeg.R("09")^1) * lpeg.P(" ")^0)^0 * -lpeg.P(1)
