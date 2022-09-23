970ba3fc589c4dbae3daf0af15b30aabf672e0a9
Matthew Robbetts <mrobbetts@gmail.com>
2022-09-23 11:44:20 +0100

    bapl1_l1_ex_adding-an-optional-sign-to-numerals
    Add an optional sign ("+") or ("-") to numerals.

--- interpreter.lua
+++ interpreter.lua
@@ -1,1 +1,5 @@
-p = lpeg.P(" ")^0 * lpeg.C(lpeg.R("09")^1) * lpeg.P(" ")^0 * (lpeg.Cp() * lpeg.P("+") * lpeg.P(" ")^0 * lpeg.C(lpeg.R("09")^1) * lpeg.P(" ")^0)^0 * -lpeg.P(1)
+space = lpeg.P(" ")^0
+numeral = lpeg.C(lpeg.S("+-")^-1 * lpeg.R("09")^1) * space
+op = lpeg.S("+-/*") * space
+
+p = space * numeral * (lpeg.Cp() * op * numeral)^0 * -lpeg.P(1)
