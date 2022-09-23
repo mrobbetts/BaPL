0765350b33057e317b7a14d00de7d9b784ab237a
Matthew Robbetts <mrobbetts@gmail.com>
2022-09-23 23:58:32 +0100

    bapl1_l6_ex_adding-checks-to-the-interpreter
    Add checks of "index out of range" to the interpreter.

--- interpreter.lua
+++ interpreter.lua
@@ -99,7 +99,7 @@ IDStarter = alpha + lpeg.S("_")
 IDValids  = alphanum + lpeg.S("_")
 
 -- List of reserved words.
-reservedWords = { "return", "if", "elseif", "else", "while", "and", "or" } -- [Ex]
+reservedWords = { "return", "if", "elseif", "else", "while", "and", "or", "new" }
 
 -- Build a pattern to match any reserved word.
 reservedPattern = lpeg.P(false)
@@ -138,16 +138,17 @@ opA = lpeg.C(lpeg.S("+-")) * space
 opM = lpeg.C(lpeg.S("*/%")) * space
 opE = lpeg.C(lpeg.S("^")) * space
 opC = lpeg.C(lpeg.P("<=") + "<" + ">=" + ">" + "!=" + "==") * space
--- opL = lpeg.C(lpeg.P("and")) * space       -- [Ex]
 
+new1 = lpeg.V("new1")
+lhs  = lpeg.V("lhs")
 fact = lpeg.V("fact")
 expU = lpeg.V("expU")
 expE = lpeg.V("expE")
 expM = lpeg.V("expM")
 expA = lpeg.V("expA")
 expC = lpeg.V("expC")
-expL = lpeg.V("expL")                      -- [Ex]
-expr = lpeg.V("expr")                      -- [Ex]
+expL = lpeg.V("expL")
+expr = lpeg.V("expr")
 if1  = lpeg.V("if1")
 elif = lpeg.V("elif")
 while1 = lpeg.V("while1")
@@ -157,23 +158,28 @@ statements = lpeg.V("statements")
 
 grammar = lpeg.P{
   "statements",
-  fact = number + (T("(") * expr * T(")")) + var,
+  fact = number + (T("(") * expr * T(")")) + lhs,
   expU = ((opU * fact)                  / node("unop", "op", "exp")) + fact,
   expE = lpeg.Ct(expU * (opE * expU)^0) / foldBin,
   expM = lpeg.Ct(expE * (opM * expE)^0) / foldBin,
   expA = lpeg.Ct(expM * (opA * expM)^0) / foldBin,
   expC = lpeg.Ct(expA * (opC * expA)^0) / foldBin,
-  expL = ((expC * (Rw("and") * expL)) / node("and1", "e1", "e2")) + ((expC * (Rw("or") * expL)) / node("or1", "e1", "e2")) + expC,  -- [Ex]
+  expL = ((expC * (Rw("and") * expL)) / node("and1", "e1", "e2"))
+       + ((expC * (Rw("or")  * expL)) / node("or1", "e1", "e2"))
+       + expC,
   expr = expL,
   elif = ((Rw("elseif") * expr * block) * (elif + Rw("else") * block)^-1) / node("if1", "cond", "th", "el"),
   if1  = ((Rw("if")     * expr * block) * (elif + Rw("else") * block)^-1) / node("if1", "cond", "th", "el"),
   while1 = (Rw("while") * expr * block) / node("while1", "cond", "body"),
+  lhs  = ((var * T("[") * expr * T("]")) / node("indexed", "array", "index"))
+       + var,
+  new1 = (Rw("new") * T("[") * expr * T("]")) / node("new", "exp"),
   block = T("{") * statements * T("}"),
   statement = block
             + if1
             + while1
             + ((T("@") * expr)          / node("print", "exp"))
-            + ((ID * T("=") * expr)     / node("assgn", "id", "exp"))
+            + ((lhs * T("=") * (new1 + expr)) / node("assgn", "lhs", "exp"))
             + ((Rw("return") * expr)    / node("ret", "exp")),
   statements = (statement * (T(";") * statements)^-1 * T(";")^-1) / seqNode
 }
@@ -253,35 +259,56 @@ function Compiler:codeExp(ast)
     self:codeExp(ast.e1)
     self:codeExp(ast.e2)
     self:addCode(self.binops[ast.op])
-  elseif (ast.tag == "and1") then                -- [Ex]
-      self:codeExp(ast.e1)                       -- [Ex]
-      local l1 = self:addJump("jmpzp")           -- [Ex]
-      self:codeExp(ast.e2)                       -- [Ex]
-                                                 -- [Ex]
-      -- Fix l1 to jump to the end of the `and`. -- [Ex]
-      self.code[l1] = #(self.code) - l1          -- [Ex]
-  elseif (ast.tag == "or1") then                 -- [Ex]
-      self:codeExp(ast.e1)                       -- [Ex]
-      local l1 = self:addJump("jmpnzp")          -- [Ex]
-      self:codeExp(ast.e2)                       -- [Ex]
-                                                 -- [Ex]
-      -- Fix l1 to jump to the end of the `or`.  -- [Ex]
-      self.code[l1] = #(self.code) - l1          -- [Ex]
+  elseif (ast.tag == "and1") then
+      self:codeExp(ast.e1)
+      local l1 = self:addJump("jmpzp")
+      self:codeExp(ast.e2)
+
+      -- Fix l1 to jump to the end of the `and`.
+      self.code[l1] = #(self.code) - l1
+  elseif (ast.tag == "or1") then
+      self:codeExp(ast.e1)
+      local l1 = self:addJump("jmpnzp")
+      self:codeExp(ast.e2)
+
+      -- Fix l1 to jump to the end of the `or`.
+      self.code[l1] = #(self.code) - l1
   elseif (ast.tag == "unop") then
     self:codeExp(ast.exp)
     self:addCode(self.unops[ast.op])
   elseif (ast.tag == "variable") then
     self:addCode("load")
     self:addCode(self:id2num(ast.val))
+  elseif (ast.tag == "indexed") then
+    self:codeExp(ast.array)
+    self:codeExp(ast.index)
+    self:addCode("array_load")
+  elseif (ast.tag == "new") then
+    self:codeExp(ast.exp)
+    self:addCode("array_new")
+  end
+end
+
+function Compiler:codeAssign(ast)
+  local lhs = ast.lhs
+  if (lhs.tag == "variable") then
+    self:codeExp(ast.exp)
+    self:addCode("store")
+    self:addCode(self:id2num(lhs.val))
+  elseif (lhs.tag == "indexed") then
+    self:codeExp(lhs.array)
+    self:codeExp(lhs.index)
+    self:codeExp(ast.exp)
+    self:addCode("array_store")
+  else
+    error()
   end
 end
 
 -- Encode the root of the AST as a statement.
 function Compiler:codeStat(ast)
   if (ast.tag == "assgn") then
-    self:codeExp(ast.exp)
-    self:addCode("store")
-    self:addCode(self:id2num(ast.id))
+    self:codeAssign(ast)
   elseif (ast.tag == "seq") then
     self:codeStat(ast.st1)
     self:codeStat(ast.st2)
@@ -339,6 +366,13 @@ function Compiler:compile(ast)
   return self.code
 end
 
+function printableValue(v)
+  if (type(v) == "table") then
+    return "array[" .. v.size .. "]"
+  else
+    return v
+  end
+end
 
 function run(code, mem, stack)
   pc = 1
@@ -349,7 +383,7 @@ function run(code, mem, stack)
       pc  = pc + 1    -- we consume two code words for a push.
       top = top + 1
       stack[top] = code[pc]
-      logStr = logStr .. "\npush " .. code[pc]
+      logStr = logStr .. "\npush        " .. code[pc]
     elseif code[pc] == "load" then
       pc = pc + 1     -- we consume two code words for a load.
       top = top + 1
@@ -361,122 +395,142 @@ function run(code, mem, stack)
         error("Variable used before it is defined, at [PC " .. pc .. "] (variable " .. id ..")")
       else
         stack[top] = mem[id]
-        logStr = logStr .. "\nload " .. code[pc]
+        -- logStr = logStr .. "\nload    " .. code[pc] .. " -> " .. mem[id]
+        logStr = logStr .. "\nload        " .. code[pc] .. " -> " .. printableValue(mem[id])
+        -- logStr = logStr .. "\nload    " .. code[pc]
       end
     elseif code[pc] == "store" then
       pc = pc + 1
       id = code[pc]
       mem[id] = stack[top]
       top = top - 1
-      logStr = logStr .. "\nstore " .. code[pc]
+      logStr = logStr .. "\nstore       " .. code[pc] .. " <- " .. printableValue(mem[id])
+    elseif code[pc] == "array_new" then
+      stack[top] = { size = stack[top] }
+      logStr = logStr .. "\narray_new   " .. code[pc - 1]
+    elseif code[pc] == "array_load" then
+      local index = stack[top]
+      local array = stack[top - 1]
+      if index > array.size then                                                           -- [Ex]
+        error("Index (" .. index .. ") out of range (array size is " .. array.size .. ")") -- [Ex]
+      else
+        logStr = logStr .. "\narray_load  " .. (top - 1) .. "[" .. index .. "]" .. " -> " .. array[index]
+        top = top - 1
+        stack[top] = array[index]
+      end
+    elseif code[pc] == "array_store" then
+      local value = stack[top]
+      local index = stack[top - 1]
+      local array = stack[top - 2]
+      if index > array.size then                                                           -- [Ex]
+        error("Index (" .. index .. ") out of range (array size is " .. array.size .. ")") -- [Ex]
+      else
+        array[index] = value
+        logStr = logStr .. "\narray_store " .. top - 2 .. "[" .. index .. "]" .. " <- " .. value
+        top = top - 3
+      end
     elseif code[pc] == "return" then
-      logStr = logStr .. "\nreturn " .. tostring(stack[top])
+      logStr = logStr .. "\nreturn  " .. tostring(stack[top])
       return logStr
     elseif code[pc] == "print" then
-      logStr = logStr .. "\nprint: " .. stack[top]
+      logStr = logStr .. "\nprint       " .. stack[top]
       print(stack[top])
       top = top - 1
     elseif code[pc] == "jmpz" then
-      -- print("jmpz")
       top = top - 1
       -- if (stack[top + 1] == 0) then -- perform the jump / condition is false
       -- if (not stack[top + 1]) then -- perform the jump / condition is false
       if (not stack[top + 1] or stack[top + 1] == 0) then -- perform the jump / condition is false
-        logStr = logStr .. "\njmp " .. code[pc + 1]
+        logStr = logStr .. "\njmp         " .. code[pc + 1]
         pc = pc + code[pc + 1] + 1
       else
-        logStr = logStr .. "\nnop "
+        logStr = logStr .. "\nnop"
         pc = pc + 1
       end
-      -- print("PC after jump: " .. pc)
     elseif code[pc] == "jmp" then
-      logStr = logStr .. "\njmp " .. code[pc + 1]
+      logStr = logStr .. "\njmp         " .. code[pc + 1]
       pc = pc + code[pc + 1] + 1
-      -- print("PC after jump: " .. pc)
-    elseif code[pc] == "jmpzp" then                                            -- [Ex]
-      -- Implements short-circuiting `and`. Evaluates first operand, and:      -- [Ex]
-      -- - If non-false, pops the result and continues (to second operand).    -- [Ex]
-      -- - If false, leaves the result in place and jumps over the second.     -- [Ex]
-      if (not stack[top] or stack[top] == 0) then                              -- [Ex]
-        -- Perform the jump to the address in the next code word.              -- [Ex]
-        logStr = logStr .. "\njmpzp: " .. code[pc + 1]                         -- [Ex]
-        pc = pc + code[pc + 1] + 1                                             -- [Ex]
-      else                                                                     -- [Ex]
-        -- Pop the stack (we know is non-false) and continue.                  -- [Ex]
-        logStr = logStr .. "\njmpzp: nop"                                      -- [Ex]
-        top = top - 1                                                          -- [Ex]
-        pc = pc + 1                                                            -- [Ex]
-      end                                                                      -- [Ex]
-    elseif code[pc] == "jmpnzp" then                                           -- [Ex]
-      -- Implements short-circuiting `or`. Evaluates first operand, and:       -- [Ex]
-      -- - If non-false, leaves the result in place and jumps over the second. -- [Ex]
-      -- - If false, pops the result and continues (to second operand).        -- [Ex]
-      if (stack[top] and stack[top] ~= 0) then                                 -- [Ex]
-        -- Perform the jump to the address in the next code word.              -- [Ex]
-        logStr = logStr .. "\njmpnzp: " .. code[pc + 1]                        -- [Ex]
-        pc = pc + code[pc + 1] + 1                                             -- [Ex]
-      else                                                                     -- [Ex]
-        -- Pop the stack (we know is non-false) and continue.                  -- [Ex]
-        logStr = logStr .. "\njmpzp: nop"                                      -- [Ex]
-        top = top - 1                                                          -- [Ex]
-        pc = pc + 1                                                            -- [Ex]
+    elseif code[pc] == "jmpzp" then
+      -- Implements short-circuiting `and`. Evaluates first operand, and:
+      -- - If non-false, pops the result and continues (to second operand).
+      -- - If false, leaves the result in place and jumps over the second.
+      if (not stack[top] or stack[top] == 0) then
+        -- Perform the jump to the address in the next code word.
+        logStr = logStr .. "\njmpzp       " .. code[pc + 1]
+        pc = pc + code[pc + 1] + 1
+      else
+        -- Pop the stack (we know is non-false) and continue.
+        logStr = logStr .. "\njmpzp       nop"
+        top = top - 1
+        pc = pc + 1
+      end
+    elseif code[pc] == "jmpnzp" then
+      -- Implements short-circuiting `or`. Evaluates first operand, and:
+      -- - If non-false, leaves the result in place and jumps over the second.
+      -- - If false, pops the result and continues (to second operand).
+      if (stack[top] and stack[top] ~= 0) then
+        -- Perform the jump to the address in the next code word.
+        logStr = logStr .. "\njmpnzp      " .. code[pc + 1]
+        pc = pc + code[pc + 1] + 1
+      else
+        -- Pop the stack (we know is non-false) and continue.
+        logStr = logStr .. "\njmpzp       nop"
+        top = top - 1
+        pc = pc + 1
       end
     elseif code[pc] == "add" then
       top = top - 1
-      logStr = logStr .. "\nadd: " .. stack[top] .. " + " .. stack[top + 1]
+      logStr = logStr .. "\nadd         " .. stack[top] .. " + " .. stack[top + 1]
       stack[top] = stack[top] + stack[top + 1]
     elseif code[pc] == "sub" then
       top = top - 1
-      logStr = logStr .. "\nsub: " .. stack[top] .. " - " .. stack[top + 1]
+      logStr = logStr .. "\nsub         " .. stack[top] .. " - " .. stack[top + 1]
       stack[top] = stack[top] - stack[top + 1]
     elseif code[pc] == "mul" then
       top = top - 1
-      logStr = logStr .. "\nmul: " .. stack[top] .. " * " .. stack[top + 1]
+      logStr = logStr .. "\nmul         " .. stack[top] .. " * " .. stack[top + 1]
       stack[top] = stack[top] * stack[top + 1]
     elseif code[pc] == "div" then
       top = top - 1
-      logStr = logStr .. "\ndiv: " .. stack[top] .. " / " .. stack[top + 1]
+      logStr = logStr .. "\ndiv:        " .. stack[top] .. " / " .. stack[top + 1]
       stack[top] = stack[top] / stack[top + 1]
     elseif code[pc] == "mod" then
       top = top - 1
       stack[top] = stack[top] % stack[top + 1]
-      logStr = logStr .. "\nmod: " .. stack[top] .. " % " .. stack[top + 1]
+      logStr = logStr .. "\nmod         " .. stack[top] .. " % " .. stack[top + 1]
     elseif code[pc] == "exp" then
       top = top - 1
-      logStr = logStr .. "\nexp: " .. stack[top] .. " ^ " .. stack[top + 1]
+      logStr = logStr .. "\nexp         " .. stack[top] .. " ^ " .. stack[top + 1]
       stack[top] = stack[top] ^ stack[top + 1]
     elseif code[pc] == "neg" then
-      logStr = logStr .. "\nneg: " .. "-" .. stack[top]
+      logStr = logStr .. "\nneg         " .. "-" .. stack[top]
       stack[top] = -stack[top]
     elseif code[pc] == "not" then
-      logStr = logStr .. "\nnot: " .. "!(" .. stack[top] .. ")"
-      -- stack[top] = not stack[top]
+      logStr = logStr .. "\nnot         " .. "!(" .. stack[top] .. ")"
       if (not stack[top]) then stack[top] = 1 else stack[top] = 0 end
     elseif code[pc] == "lte" then
       top = top - 1
-      logStr = logStr .. "\nlte: " .. stack[top] .. " <= " .. stack[top + 1]
+      logStr = logStr .. "\nlte         " .. stack[top] .. " <= " .. stack[top + 1]
       stack[top] = stack[top] <= stack[top + 1]
     elseif code[pc] == "lt" then
       top = top - 1
-      logStr = logStr .. "\nlt: " .. stack[top] .. " < " .. stack[top + 1]
-      print("lt: " .. stack[top] .. " < " .. stack[top + 1] .. "?" )
+      logStr = logStr .. "\nlt          " .. stack[top] .. " < " .. stack[top + 1]
       stack[top] = stack[top] < stack[top + 1]
-      print("after lt: " .. tostring(stack[top]))
     elseif code[pc] == "gte" then
       top = top - 1
-      logStr = logStr .. "\ngte: " .. stack[top] .. " >= " .. stack[top + 1]
+      logStr = logStr .. "\ngte          " .. stack[top] .. " >= " .. stack[top + 1]
       stack[top] = stack[top] >= stack[top + 1]
     elseif code[pc] == "gt" then
       top = top - 1
-      logStr = logStr .. "\ngt: " .. stack[top] .. " < " .. stack[top + 1]
+      logStr = logStr .. "\ngt          " .. stack[top] .. " > " .. stack[top + 1]
       stack[top] = stack[top] > stack[top + 1]
     elseif code[pc] == "neq" then
       top = top - 1
-      logStr = logStr .. "\nneq: " .. stack[top] .. " != " .. stack[top + 1]
+      logStr = logStr .. "\nneq         " .. stack[top] .. " != " .. stack[top + 1]
       stack[top] = stack[top] ~= stack[top + 1]
     elseif code[pc] == "eq" then
       top = top - 1
-      logStr = logStr .. "\neq: " .. stack[top] .. " == " .. stack[top + 1]
+      logStr = logStr .. "\neq          " .. stack[top] .. " == " .. stack[top + 1]
       stack[top] = stack[top] == stack[top + 1]
     else
       error("Unknown instruction: " .. code[pc])
