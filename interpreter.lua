ab355532033fa05c6060678b665b158396500e0d
Matthew Robbetts <mrobbetts@gmail.com>
2022-09-23 11:46:21 +0100

    bapl1_l1_ex_adding-more-operators
    a) Add a remainder operator ('%') to the language, with the same priority as the other multiplicative operators.
    b) Add an exponential operator ('^') to the language, with a higher priority than the multiplicative operators. Use the same concepts we used when we added multiplicative operators. You will probably need a new kind of expression, besides 'term' and 'exp'.

--- interpreter.lua
+++ interpreter.lua
@@ -1,5 +1,33 @@
-space = lpeg.P(" ")^0
-numeral = lpeg.C(lpeg.S("+-")^-1 * lpeg.R("09")^1) * space
-op = lpeg.S("+-/*") * space
+lpeg = require "lpeg"
+-- pt = require "pt"
 
-p = space * numeral * (lpeg.Cp() * op * numeral)^0 * -lpeg.P(1)
+space = lpeg.P(" ")^0
+numeral = (lpeg.R("09")^1 / tonumber) * space
+
+opE = lpeg.C(lpeg.S("^")) * space
+opM = lpeg.C(lpeg.S("*/%")) * space
+opA = lpeg.C(lpeg.S("+-")) * space
+
+function fold(lst)
+  acc = lst[1]
+  for i = 2, #lst, 2 do
+    if (lst[i] == "+") then
+      acc = acc + lst[i + 1]
+    elseif (lst[i] == "-") then
+      acc = acc - lst[i + 1]
+    elseif (lst[i] == "*") then
+      acc = acc * lst[i + 1]
+    elseif (lst[i] == "/") then
+      acc = acc / lst[i + 1]
+    elseif (lst[i] == "%") then
+      acc = acc % lst[i + 1]
+    elseif (lst[i] == "^") then
+      acc = acc ^ lst[i + 1]
+    end
+  end
+  return acc
+end
+
+exp = space * lpeg.Ct(numeral * (opE * numeral)^0) / fold
+term = space * lpeg.Ct(exp * (opM * exp)^0) / fold
+sum = space * (lpeg.Ct(term * (opA * term)^0) / fold) * -lpeg.P(1)
